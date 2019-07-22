0284267faeb6442c4a540c9505a715acecdfb83a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-22 06:43:04 +0000

    Deploy SamiUrias/react-porftolio-2 to github.com/SamiUrias/react-porftolio-2.git:master

---
+++ precache-manifest.4b8492920c4407b788481f734a1ed6ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4716ec5c3f999248f7e7f9a45b3ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b58423fda6661a751b2",
+    "url": "/static/js/2.62bd693e.chunk.js"
+  },
+  {
+    "revision": "f46a782ff19f0e1e430f",
+    "url": "/static/js/main.cb92d1d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
